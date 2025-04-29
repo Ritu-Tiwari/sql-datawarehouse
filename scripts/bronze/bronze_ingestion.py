3ac1ea80467aa589f39b8f5e474c142025abd0c9
Ritu Tiwari <ritzzz.t@gmail.com>
2025-04-29 18:52:25 +0530

    Create bronze_ingestion.py

---
+++ scripts/bronze/bronze_ingestion.py
@@ -0,0 +1,37 @@
+'''
+This script automates data ingestion into the bronze layer from source folders - crm and erm . 
+Each file is loaded into the dataframe and ingested into the appropriate database table.
+Warning -It assumes that the datasets folder is in the same directory as the script.
+
+'''
+
+
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+from  pathlib import Path
+
+load_dotenv()
+
+user = os.environ["MYSQL_USER"]
+password = os.environ["MYSQL_PASSWORD"] 
+host = os.environ["HOST"]
+dbname = os.environ["DATABASE"]
+
+engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{dbname}')
+
+dataset_dir = Path("datasets")
+source_dirs = [ d for d in dataset_dir.iterdir() if d.is_dir()]
+
+try:
+    for dir in source_dirs:
+        source = dir.name.split("_")[1]
+        for file in dir.iterdir():
+            df = pd.read_csv(file)
+            table = f"bronze_{source}_{file.stem.lower()}"
+            df.to_sql(name=table, con=engine, if_exists="append", index=False)
+            print(f"{file.name} ingested into table '{table}' successfully.")
+            
+except Exception as e:
+    print(f"Error ingesting {file.name}: {e}")
